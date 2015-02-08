4a8faee8b69f18a1d5c05b6cb9ab42ac7156d926
Antonio Howcroft Ferreira <howcroft@gmail.com>
2015-02-08 23:55:36 +0000

    Re-organized and changed subset

--- plot1.R
+++ plot1.R
@@ -1,22 +1,23 @@
 #plot 1
-data <- read.csv("./household_power_consumption.txt", sep = ";");
+data <- read.csv("./household_power_consumption.txt", sep = ";", na.string="?");
 
-#Subset and sanitize data
-test <- strptime(data[1:length(data[,1]), 1], "%d/%m/%Y")
+
+#Subset
+data <- subset(data, data$Date == "1/2/2007" | data$Date =="2/2/2007")
+
+
+#Sanitize data
+test <- strptime(data[1:length(data[,1]), 1], "%d/%m/%Y %H:%M:%S")
 dataFrameTest <- data.frame(test)
-data[, 1] = dataFrameTest[,1]
+data[, 1] <- dataFrameTest[,1]
 rm(dataFrameTest)
 rm(test)
+rm(dataFilter)
 
-dataFilter <- subset(data, data$Date>=" 2007-02-01 00:00:00" & data$Date<=" 2007-02-02 23:59:00")
-rm(data)
 
 #Prepare plot
-histX <- as.numeric(dataFilter$Global_active_power)/1000
+histX <- as.numeric(data$Global_active_power)
 # png default parameters are: width = 480, height = 480 so no need to override
 png(filename="plot1.png")
-hist(histX, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red", axes=F)
-axis(2)
-#axis(1, at=as.vector(c(0,2,4,6))) doesn't wo
-axis(1, xlim=c(0,6), at=as.vector(c(0,2,4,6)))
+hist(histX, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
 dev.off()
