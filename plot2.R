90d3300f10b42a5f59d005df3f2482edad83d4ee
Antonio Howcroft Ferreira <howcroft@gmail.com>
2015-02-09 00:18:54 +0000

    cleared plot2

--- plot2.R
+++ plot2.R
@@ -1,30 +1,19 @@
 #plot2
-data <- read.csv("./household_power_consumption.txt", sep = ";");
+data <- read.csv("./household_power_consumption.txt", sep = ";", na.string="?");
 
-#Subset and sanitize data
-test <- strptime(data[1:length(data[,1]), 1], "%d/%m/%Y")
-dataFrameTest <- data.frame(test)
-data[, 1] = dataFrameTest[,1]
-rm(dataFrameTest)
-rm(test)
 
-dataFilter <- subset(data, data$Date>="2007-02-01 00:01:00" & data$Date<=" 2007-02-02 23:59:00")
-rm(data)
+
+#Subset
+data <- subset(data, data$Date == "1/2/2007" | data$Date =="2/2/2007")
+
+
+#Sanitize data
+data$Time <- strptime(paste(data$Date,data$Time), format = "%d/%m/%Y %H:%M:%S")
+
 
 #Prepare plot
 # png default parameters are: width = 480, height = 480 so no need to override
-test <- strptime(dataFilter[1:length(dataFilter[,2]), 2], "%H:%M:%S")
-dataFrameTest <- data.frame(test)
-dataFilter[, 2] = dataFrameTest[,1]
-rm(dataFrameTest)
 #weekdays(strptime("2007-02-02 01:01:00", "%Y-%m-%d %H:%M:%S"))
-#temp <- dataFilter[c(1,702,1439),2]
-#temp <- lapply(temp, function(x)weekdays(x, abbreviate=T))
-
 png(filename="plot2.png")
-#hist(histX, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red", )
-with( dataFilter, plot(Time, Global_active_power, type = "l", main="", ylab = "Global Active Power (kilowatts)", axes=F))
-axis(2)
-axis(1)
+plot(data$Time, data$Global_active_power, type = "l", main="", xlab ="", ylab = "Global Active Power (kilowatts)")
 dev.off()
-
